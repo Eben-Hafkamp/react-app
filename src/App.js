b0de3528ecd3e12e4b9619a99f8ee06a4e790c69
Eben <ebenhafkamp@gmail.com>
2018-02-24 13:52:24 +1300

    Flexible Lists

--- src/App.js
+++ src/App.js
@@ -6,42 +6,76 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     persons: [
-      { name: 'Eben', age: 24 },
-      { name: 'Mabel', age: 52 }
-    ]
+      { id: '2342342', name: 'Eben', age: 24 },
+      { id: '2341523', name: 'Mabel', age: 52 }
+    ],
+    show_persons: true
   }
 
-  switch_name_handler = (new_name) => {
+  name_changed_handler = (person_id, event) => {
+    const person_index = this.state.persons.findIndex((p) => {
+      return p.id === person_id;
+    }); 
+    // find and save the index of the array of the person id that was passed in
+
+    const person = {
+      ...this.state.persons[person_index]     // make a copy of the person we want to change
+    };                                        // in an object format, from the index number
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];  // make a copy of the entire persons object
+    
+    persons[person_index] = person;           // replace the target person with the new one we made above
+
     this.setState({
-      persons: [
-        { name: 'Bleep Bloop', age: 25 },
-        { name: new_name, age: 22 }
-      ]
+      persons: persons
     });
   }
 
-  name_changed_handler = (event) => {
+  show_persons_handler = () => {
+    let is_showing = this.state.show_persons;
     this.setState({
-      persons: [
-        { name: event.target.value, age: 24 },
-        { name: 'Mabel', age: 52 }
-      ]
+      show_persons: !is_showing
     });
+  }
+
+
+  delete_person_handler = (index) => {
+    const persons = this.state.persons;
+    persons.splice(index, 1);
+    this.setState({ persons: persons });
   }
 
   render() {
+
+    let persons = null;
+
+    if (this.state.show_persons) {
+      persons = (
+        <div>
+          {
+            this.state.persons.map((person, index) => {
+              return <Person 
+                      name={ person.name } 
+                      age={ person.age }
+                      change={ (event) => this.name_changed_handler(person.id, event) } 
+                      // same as function (event) { this.name_changed_handler(person.id, event) }
+                      // in the execution context this function is running and invokes a new function
+                      // whilst passing the event 
+                      click={ () => this.delete_person_handler(index) }
+                      key={ person.id } />
+            })
+          }
+        </div>
+      );
+    }
+
     return (
       <div className="App">
         <h1>Hello, I am a React App :P</h1>
-        <button onClick={ this.switch_name_handler.bind(this, 'Maypoo') }>Switch Name</button>
-        <Person 
-        name={ this.state.persons[0].name } 
-        age={ this.state.persons[0].age }
-        change={ this.name_changed_handler } />
-        <Person 
-        name={ this.state.persons[1].name } 
-        age={ this.state.persons[1].age }
-        click={ this.switch_name_handler.bind(this, 'M.a.b.e.l') }>I like to sleep!</Person>
+        <button onClick={ this.show_persons_handler }>Toggle Persons</button>
+        { persons }
       </div>
     );
   }
